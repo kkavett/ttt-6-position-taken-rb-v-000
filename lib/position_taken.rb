b7d3c6195d23420b8da26833cb34b68dc07e1505
Katie Kavett <katie.kavett@flatironschool.com>
2018-09-03 22:19:31 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -8,7 +8,7 @@ def position_taken?(board,index)
     false
   elsif board[index] == nil
     false
-  elsif board[index] == ("X" || "O")
+  elsif board[index] == "X" || "O"
     true
   else
     false
